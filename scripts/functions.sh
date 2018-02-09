5aba81a82222bbe7afec62bbf9bab20982bc140a
Edwin Bühler <ed@mac.domain_not_set.invalid>
2018-02-09 10:16:30 +0100

    fujctions script

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -23,12 +23,11 @@ function quit {
 }
 
 function start {
-  echo "😴💤 Sleeping function start server"
-	sleep $sleepTime
   echo -e "🛠 Starting first server."
   dgraph server --memory_mb 2048 --zero localhost:5082 -o 2
   # Wait for membership sync to happen.
   
+  sleep $sleepTime
   return 0
 }
 
@@ -37,16 +36,20 @@ function startZero {
   dgraph zero --port_offset -1998
   # To ensure dgraph doesn't start before dgraphzero.
 	# It takes time for zero to start on travis(mac).
-  echo -e "🛠 dgraph zero is started ------------------------------------- \n"
-  #echo "😴💤 Sleeping"
+  echo -e "🛠 dgraph zero ios started ------------------------------------- \n"
+  echo "😴💤 Sleeping"
 	sleep $sleepTime
+  echo -e "🛠 dgraph zero ios started ------------3------------------------- \n"
 }
 
 function testing {
-  # Wait for membership sync to happen.
-  echo "😴💤 Sleeping function testing"
-  sleep 90
   echo -e "Testing."
+  
+  # Wait for membership sync to happen.
+  echo "😴💤 Sleeping"
+  sleep $sleepTime
+  echo "😴💤 Sleeping"
+  sleep $sleepTime
   mix local.rebar --force
   mix local.hex --force
   mix deps.get
