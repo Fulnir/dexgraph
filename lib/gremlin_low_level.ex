4e2ce2f1587d986a89a0b4f2f4dee1080fbdfbb2
Edwin Bühler <edbuehler@icloud.com>
2018-03-06 17:22:05 +0100

    note in read me

--- lib/gremlin_low_level.ex
+++ lib/gremlin_low_level.ex
@@ -41,10 +41,33 @@ defmodule DexGraph.Gremlin.LowLevel do
     mutate_with_commit(~s({set{<#{subject_uid}> <#{predicate}> <#{object_uid}> .}}))
   end
 
-  @doc """
-  
-  """
-  def query_vertex(graph, vertex_uid) do
+@doc """
 
+  """
+  def query_vertex(graph, predicate, object, display) do
+    channel = graph.channel
+    if display != "expand(_all_)" do
+      display = "vertex_type " <> display
+    end
+    query = """
+    { vertices(func: anyofterms(#{predicate}, \"#{object}\")) { #{display} } }
+    """
+
+    request = ExDgraph.Api.Request.new(query: query)
+    {:ok, msg} = channel |> ExDgraph.Api.Dgraph.Stub.query(request)
+    decoded_json = Poison.decode!(msg.json)
+    vertices = decoded_json["vertices"]
+    Logger.info(fn -> "💡 vertices: #{inspect vertices}" end)
+    map =
+      Enum.map(vertices, fn vertex_map ->
+        vertex = for {key, val} <- vertex_map, into: %{}, do: {String.to_atom(key), val}
+        struct_type = String.to_existing_atom("Elixir." <> vertex.vertex_type)
+        struct(struct_type, vertex)
+      end)
+
+    map
+  end
+  def query_vertex(graph, predicate, object) do
+    query_vertex(graph, predicate, object, "expand(_all_)")
   end
 end
